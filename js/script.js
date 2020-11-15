625f3ea95a82cb55657ee22abfd13750e1bebea3
Francesco <francescofogliajj@gmail.com>
2020-11-15 17:08:15 +0100

    Commit #1

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ genera.addEventListener( 'click',
     console.log(utente);
 
     var inputChilometri = document.getElementById("chilometri");
-    var chilometri = parseInt(inputChilometri.value);
+    var chilometri = Math.abs(inputChilometri.value);
     console.log(chilometri);
 
     var inputEta = document.getElementById("eta");
@@ -29,6 +29,7 @@ genera.addEventListener( 'click',
     console.log(scontoOver);
 
 
+    // Prezzo calcolato e Categoria selezionata dall'utente (offerta)
     if (eta == "minorenne") {
       var costoBiglietto = (chilometri * prezzo) * scontoMinorenni;
       var offerta = "Sconto Minorenne";
@@ -41,20 +42,24 @@ genera.addEventListener( 'click',
     }
 
 
+    // Numero carrozza (numero casuale tra 1 e 9)
     var carrozza = Math.floor(Math.random() * 9) +1;
     console.log(carrozza)
 
+    // Codice treno (numero casuale tra 90000 e 100000 escluso)
     var codiceCP = Math.floor(Math.random() * 10000) + 90000;
     console.log(codiceCP);
 
 
+    // Output generati
     document.getElementById("passeggero").innerHTML = utente;
     document.getElementById("offerta").innerHTML = offerta;
     document.getElementById("carrozza").innerHTML = carrozza;
     document.getElementById("codice-cp").innerHTML = codiceCP;
-    document.getElementById("costo-biglietto").innerHTML = costoBiglietto;
+    document.getElementById("costo-biglietto").innerHTML = costoBiglietto.toFixed(2) + " €";
 
 
+    // Mostra biglietto generato
     var cardBiglietto = document.getElementById("card-2");
     cardBiglietto.classList.remove("hidden");
     cardBiglietto.classList.add("show");
@@ -68,16 +73,19 @@ var annulla = document.getElementById("annulla");
 annulla.addEventListener( 'click',
   function() {
 
+    // Input annullati
     document.getElementById("utente").value = "";
     document.getElementById("chilometri").value = "";
     document.getElementById("eta").value = "";
 
+    // Output annullati
     document.getElementById("passeggero").innerHTML = "";
     document.getElementById("offerta").innerHTML = "";
     document.getElementById("carrozza").innerHTML = "";
     document.getElementById("codice-cp").innerHTML = "";
     document.getElementById("costo-biglietto").innerHTML = "";
 
+    // Nasconde biglietto annullato
     var cardBiglietto = document.getElementById("card-2");
     cardBiglietto.classList.remove("show");
     cardBiglietto.classList.add("hidden");
